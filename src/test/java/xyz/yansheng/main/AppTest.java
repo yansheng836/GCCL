d62fa11ba5f912c82197096124a1699cd2be3564
yansheng836 <sheng.yan836@gmail.com>
2019-10-13 23:28:33 +0800

    :white_check_mark:Updating tests for main.

--- src/test/java/xyz/yansheng/main/AppTest.java
+++ src/test/java/xyz/yansheng/main/AppTest.java
@@ -1,5 +1,7 @@
 package xyz.yansheng.main;
 
+import java.io.ByteArrayInputStream;
+
 import org.junit.Test;
 
 /**
@@ -13,7 +15,19 @@ public class AppTest {
      */
     @Test
     public void testMain() {
-        // App.main(null);
-    }
+        
+        // 第1个用户名正确，23都错误
+        String username1 = "weixin_41287260";
+        String username2 = "weixin_412872";
+        String username3 = "weixin_";
 
+        String[] usernames = {username1, username2, username3};
+        for (String username : usernames) {
+
+            // System.setIn(InputStream in) :重新分配“标准”输入流。
+            System.setIn(new ByteArrayInputStream(username.getBytes()));
+            App.main(null);
+        }
+        
+    }
 }
