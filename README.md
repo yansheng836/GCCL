ca169ea4b5d9449d182ed37048a1e60f624b8170
yansheng836 <sheng.yan836@gmail.com>
2019-10-15 15:10:39 +0800

    :white_check_mark:Updating tests for main.
    测试多种不同情况的账号。

--- README.md
+++ README.md
@@ -36,6 +36,10 @@
 
 请看生成的文件 [CSDN博客目录-2019-10-13.14-15-36.md](<https://github.com/yansheng836/GCCL/blob/master/CSDN博客目录-2019-10-13.14-15-36.md>)，或者参考我的CSDN博客目录：[【目录】博客目录 | 先点这里](<https://blog.csdn.net/weixin_41287260/article/details/101735143>)。
 
+## 测试
+
+对多种情况进行了测试，得出了对应的程序运行时间，详见：[程序测试](<https://github.com/yansheng836/GCCL/blob/master/程序测试>)。
+
 ## 使用
 
 ### 简单使用
--- src/test/java/xyz/yansheng/main/AppTest.java
+++ src/test/java/xyz/yansheng/main/AppTest.java
@@ -13,17 +13,44 @@ import org.junit.Test;
 public class AppTest {
 
     /**
+     * 测试多组数据（包含多种情况）
+     * 
      * Test method for {@link xyz.yansheng.main.App#main(java.lang.String[])}.
      */
     @Test
     public void testMain() {
 
-        // 第3个用户名正确，12都错误
-        String username1 = "weixin_";
-        String username2 = "weixin_412872";
-        String username3 = "weixin_41287260";
+        // 0X（即测试编号0开头）：CSDN排名前几的博主。
+        String username01 = "stpeace";
+        String username02 = "phphot";
 
-        String[] usernames = {username1, username2, username3};
+        // 1X：随机选的博客进行测试。
+        String username10 = "weixin_41287260";
+        String username11 = "yilovexing";
+        String username12 = "hjx9452";
+        String username13 = "zhouzhiwengang";
+        String username14 = "blogdevteam";
+
+        // 2X：可能会有问题的测试：博客主页没有显示分类专栏。
+        String username20 = "weixin_30892987";
+        String username21 = "lingshengxueyuan";
+        String username22 = "love_caicai";
+
+        // 3X：可能会有问题的测试：分类专栏有层级（即有二级分类）。
+        String username30 = "qing_gee";
+
+        // 4X：错误的用户名。
+        String username40 = "weixin_41287";
+
+        // 5X：可能会有问题的测试：自定义域名，用户名形式为：https: //username.blog.csdn.net/
+        // ，如：<https://wuyeliang.blog.csdn.net/>。
+        String username50 = "wuyeliang";
+        String username51 = "coderfix";
+
+        String[] usernames =
+            {username01, username02, username10, username11, username12, username13, username14,
+                username20, username21, username22, username30, username40, username50, username51};
+
         for (String username : usernames) {
             // System.setIn(InputStream in) :重新分配“标准”输入流。
             System.setIn(new ByteArrayInputStream(username.getBytes()));
