8bf29d51b976a3cbfb1bfd51ba8462aadca74d92
yansheng836 <sheng.yan836@gmail.com>
2022-10-06 00:10:10 +0800

    fix: change the category's url

--- src/main/java/xyz/yansheng/util/SpiderUtil.java
+++ src/main/java/xyz/yansheng/util/SpiderUtil.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,19 +16,19 @@ import xyz.yansheng.bean.Category;
 
 /**
  * 爬虫工具类
- * 
+ *
  * @author yansheng
  * @date 2019/09/30
  */
 public class SpiderUtil {
 
     public static final String UTF8 = "UTF-8";
+    public static final int PER_PAGE_COUNT = 40;
 
     /**
      * 获取该用户的非空的分类列表。（注意这里要进行异常判断，如用户名输入错误等。后面不在进行处理。）
-     * 
-     * @param username
-     *            用户名
+     *
+     * @param username 用户名
      * @return 分类专栏列表。如果是账号有问题返回null（用object==null判断）,如果是爬取的网页元素有问题返回categoryList（用list.isEmpty()判断）
      */
     public static ArrayList<Category> getCategoryList(String username) {
@@ -35,7 +36,7 @@ public class SpiderUtil {
         // 用户博客主页地址
         String url = "https://blog.csdn.net/" + username;
 
-        ArrayList<Category> categoryList = new ArrayList<Category>(20);
+        ArrayList<Category> categoryList = new ArrayList<Category>(PER_PAGE_COUNT);
 
         Document doc = null;
         try {
@@ -67,7 +68,7 @@ public class SpiderUtil {
                     String href = liElement.attr("href");
                     String title = titleElement.text();
                     String countString = countElement.text();
-                    countString = countString.replace("篇","");
+                    countString = countString.replace("篇", "");
                     Integer count = Integer.parseInt(countString);
                     // System.out.println("count:" + count);
                     num = num + count;
@@ -96,9 +97,8 @@ public class SpiderUtil {
 
     /**
      * 获取该分类的所有博客列表（1页20篇博客，可能有多页）
-     * 
-     * @param category
-     *            分类实体类（blogs为空）
+     *
+     * @param category 分类实体类（blogs为空）
      * @return 博客列表（出现问题、异常返回null）
      */
     public static ArrayList<Blog> getCategoryBlogs(Category category) {
@@ -106,23 +106,24 @@ public class SpiderUtil {
         Integer count = category.getCount();
         // 求该分类专栏的博客有多少页，一页有20篇博客，如果有21篇，就说明有2页
         // 2021年7月31日20:50:33更新 没有再按照分页显示，全部一页加载 https://blog.csdn.net/weixin_41287260/category_8128217.html
-        int maxPageCount = 20;
-        int blogPage = count / maxPageCount;
-        if (count % maxPageCount != 0) {
+//        int maxPageCount = 20;
+        int blogPage = count / PER_PAGE_COUNT;
+        if (count % PER_PAGE_COUNT != 0) {
             blogPage++;
         }
+        System.out.println("----专栏:【" + category.getTitle()+"】的文章数量为:" + count);
         // System.out.println("\n\n该分类专栏的博客页数：" + blogPage);
 
         ArrayList<Blog> blogs = new ArrayList<Blog>(count);
         String url = category.getUrl();
-
+//        System.out.println("url:" + url);
         // 循环爬取每页的博客信息
         for (int i = 1; i <= blogPage; i++) {
-            String pageUrl = url + "/" + i;
-            
-            ArrayList<Blog> pageBlogs = new ArrayList<Blog>(maxPageCount);
-            // pageBlogs = getPageBlogs(pageUrl);
-            pageBlogs = getPageBlogs(url);
+            String pageUrl = url.replace(".html", "") + "_" + i + ".html";
+//            System.out.println("pageUrl:" + pageUrl);
+
+            ArrayList<Blog> pageBlogs = new ArrayList<Blog>(PER_PAGE_COUNT);
+            pageBlogs = getPageBlogs(pageUrl);
 
             // 如果不为空，添加到列表；为空时，直接跳出循环。
             if (pageBlogs != null) {
@@ -135,9 +136,8 @@ public class SpiderUtil {
 
     /**
      * 查找分类的每页的博客列表信息（这个方法只爬取单页的数据）
-     * 
-     * @param pageUrl
-     *            分类专栏的网址（如果有多页，表示某一页的地址）
+     *
+     * @param pageUrl 分类专栏的网址（如果有多页，表示某一页的地址）
      * @return 博客列表（出现问题、异常返回null）
      */
     public static ArrayList<Blog> getPageBlogs(String pageUrl) {
@@ -145,14 +145,14 @@ public class SpiderUtil {
         // 定义：爬取数据出现错误时，返回的错误语句。（因为这种情况比较多，这里进行统一定义）
         String errorString = "爬取：" + pageUrl + " 时出现问题！可能是关键的页面元素发生了变化，请进行排查。";
 
-        ArrayList<Blog> blogs = new ArrayList<Blog>(20);
+        ArrayList<Blog> blogs = new ArrayList<Blog>();
 
         // 获取文档对象
         Document doc = null;
         try {
             Connection con = Jsoup.connect(pageUrl).userAgent(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
-                .timeout(30000);
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
+                    .timeout(30000);
             Connection.Response response = con.execute();
 
             int successCode = 200;
@@ -199,7 +199,7 @@ public class SpiderUtil {
 
         } catch (NullPointerException e) {
             System.err
-                .println("在获取‘分类专栏’：" + pageUrl + " 的博客的信息时发生空指针异常，可能是获取数据的关键页面元素发生了变化，请进行排查。");
+                    .println("在获取‘分类专栏’：" + pageUrl + " 的博客的信息时发生空指针异常，可能是获取数据的关键页面元素发生了变化，请进行排查。");
             return null;
         }
 
